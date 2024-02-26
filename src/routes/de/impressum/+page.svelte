76556f80316fc1de0e96484e0c6a7d724db3e45e
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-02-26 17:28:33 +0100

    Impressum DE

---
+++ src/routes/de/impressum/+page.svelte
@@ -0,0 +1,43 @@
+<div class="w-full">
+	<div class="mx-auto mt-10 w-fit">
+		<div class="uui-prose uui-desktop:uui-prose-desktop">
+			<h4>Impressum</h4>
+
+			<p>
+				Benjamin Preiss<br />
+				Strandweg, 98a<br />
+				22587 Hamburg
+			</p>
+
+			<h5>Kontakt</h5>
+			<p>
+				Telefon: +49 160 705 97 12<br />
+				E-Mail: contact@frontline.codes
+			</p>
+
+			<h5>Umsatzsteuer-ID</h5>
+			<p>
+				Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+				DE328197327
+			</p>
+
+			<h5>EU-Streitschlichtung</h5>
+			<p>
+				Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS)
+				bereit: <a
+					href="https://ec.europa.eu/consumers/odr/"
+					target="_blank"
+					rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a
+				>.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+			</p>
+
+			<h5>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h5>
+			<p>
+				Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+				Verbraucherschlichtungsstelle teilzunehmen.
+			</p>
+
+			<p>Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a></p>
+		</div>
+	</div>
+</div>
