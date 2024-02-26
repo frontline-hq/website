5979c618ef2049a196e2be28b37eaa6048bc23ba
Nadiem <nadiem8@msn.com>
2024-02-26 13:29:48 +0100

    Add mailto functionality to email us button and add CalendarPopUp.svelte

---
+++ src/lib/components/CalendarPopUp/CalendarPopUp.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { communicationPhone as phoneCall } from '@frontline-hq/untitledui-icons';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		(function (C, A, L) {
+			let p = function (a, ar) {
+				a.q.push(ar);
+			};
+			let d = C.document;
+			C.Cal =
+				C.Cal ||
+				function () {
+					let cal = C.Cal;
+					let ar = arguments;
+					if (!cal.loaded) {
+						cal.ns = {};
+						cal.q = cal.q || [];
+						d.head.appendChild(d.createElement('script')).src = A;
+						cal.loaded = true;
+					}
+					if (ar[0] === L) {
+						const api = function () {
+							p(api, arguments);
+						};
+						const namespace = ar[1];
+						api.q = api.q || [];
+						typeof namespace === 'string' ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar);
+						return;
+					}
+					p(cal, ar);
+				};
+		})(window, 'https://app.cal.com/embed/embed.js', 'init');
+		Cal('init', { origin: 'https://cal.com' });
+
+		Cal('ui', {
+			styles: { branding: { brandColor: '#000000' } },
+			hideEventTypeDetails: false,
+			layout: 'month_view'
+		});
+	});
+
+	$: calConfig = JSON.stringify({ layout: 'month_view' });
+</script>
+
+<div class="relative group">
+	<button
+		data-cal-link="frontline-meeting/vulnerability-disclosure"
+		data-cal-config={calConfig}
+		class="inset-0 rounded-full absolute bg-transparent"
+	></button>
+	<tdc-button
+		type="button"
+		class="group-hover:text-uui-button-secondary-fg_hover group-hover:bg-uui-button-secondary-bg_hover w-full"
+		tdc={{
+			size: { default: 'xl', 'uui-desktop': '2xl' },
+			destructive: 'false',
+			hierarchy: 'secondary',
+			coloring: 'gray'
+		}}
+		icon={{ type: 'icon', leading: phoneCall }}>Schedule call</tdc-button
+	>
+</div>
