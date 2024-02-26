61ced8605fb03c74df7be16377cbeda1ed9216a4
Nadiem <nadiem8@msn.com>
2024-02-26 14:32:25 +0100

    Fix small UUI_LICENSE mistake

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { UUI_LICENSE } from '$env/static/UUI';
+	import { UUI_LICENSE } from '$env/static/public';
 	import { License } from '@frontline-hq/uui/license';
 	import '../app.css';
 </script>
