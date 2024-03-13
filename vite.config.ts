339a9d36d8b630a2fbc67a8728907f7b2131be4e
Benjamin Preiss <benjaminpreiss@protonmail.com>
2024-03-13 17:31:56 +0100

    Setup inlang integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 import { plugin } from '@frontline-hq/tdc/plugin';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { imagetools } from 'vite-imagetools';
+import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite"
 
 export default defineConfig({
-	plugins: [enhancedImages(), imagetools(), await plugin(), sveltekit()]
+	plugins: [paraglide({
+		project: "./project.inlang",
+		outdir: "./src/lib/paraglide",
+	}), enhancedImages(), imagetools(), await plugin(), sveltekit()]
 });
