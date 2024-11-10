4e7127982bcb92f507562b84682195b7eb3eb783
Nadiem <nadiem8@msn.com>
2024-11-10 15:47:37 +0100

    feat: Add FeaturedNumber component for displaying featured numbers

---
+++ apps/frontline-website/src/lib/components/FeaturedNumber/FeaturedNumber.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import FeaturedNumberAtom from './FeaturedNumberAtom.svelte';
+
+	interface $$Props extends HTMLAttributes<HTMLSpanElement> {
+		url: string;
+	}
+
+	export let url: string;
+</script>
+
+<div
+	class="flex items-center bg-transparent border-uui-1 border-uui-featured-icon-modern-border justify-center rounded-uui-xl w-[2.5rem] h-[2.5rem] lg:w-[3rem] lg:h-[3rem]
+	 "
+>
+	<FeaturedNumberAtom {url}></FeaturedNumberAtom>
+</div>
