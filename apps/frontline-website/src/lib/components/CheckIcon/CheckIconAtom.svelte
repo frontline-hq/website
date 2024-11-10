a0b1bc8a587f1d600f473c5e5b042ce860fab6ed
Nadiem <nadiem8@msn.com>
2024-11-10 15:47:32 +0100

    feat: Add CheckIcon component for displaying checkmarks

---
+++ apps/frontline-website/src/lib/components/CheckIcon/CheckIconAtom.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLAttributes<HTMLSpanElement> {
+		url: string;
+	}
+
+	export let url: string;
+</script>
+
+<div
+	style="--featured-check-icon-url:url('{url}')"
+	class="
+		[mask-position:center]
+		[mask-size:contain]
+		[mask-repeat:no-repeat]
+		[mask-image:var(--featured-check-icon-url)]
+        bg-uui-featured-icon-light-fg-success
+		w-[1rem] h-[0.875rem]
+		lg:w-[1rem] lg:h-[0.875rem]
+		{$$props.class}"
+/>
